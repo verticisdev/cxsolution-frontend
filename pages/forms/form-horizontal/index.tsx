81052d07541641103dd7c47f5357cbab94e33d8f
diego_verticis <diego@verticis.com.br>
2023-12-18 15:25:40 -0300

    fix(): fix basic layout

--- pages/forms/form-horizontal/index.tsx
+++ pages/forms/form-horizontal/index.tsx
@@ -1,23 +1,23 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography } from "@mui/material";
 
 // components
-import Breadcrumb from '../../../src/layouts/full/shared/breadcrumb/Breadcrumb';
-import PageContainer from '../../../src/components/container/PageContainer';
-import ParentCard from '../../../src/components/shared/ParentCard';
-import BasicLayout from '../../../src/components/forms/form-horizontal/BasicLayout';
-import BasicIcons from '../../../src/components/forms/form-horizontal/BasicIcons';
-import FormSeparator from '../../../src/components/forms/form-horizontal/FormSeparator';
-import FormLabelAlignment from '../../../src/components/forms/form-horizontal/FormLabelAlignment';
-import CollapsibleForm from '../../../src/components/forms/form-horizontal/CollapsibleForm';
-import FormTabs from '../../../src/components/forms/form-horizontal/FormTabs';
+import Breadcrumb from "../../../src/layouts/full/shared/breadcrumb/Breadcrumb";
+import PageContainer from "../../../src/components/container/PageContainer";
+import ParentCard from "../../../src/components/shared/ParentCard";
+import BasicLayout from "../../../src/components/forms/form-horizontal/BasicLayout";
+import BasicIcons from "../../../src/components/forms/form-horizontal/BasicIcons";
+import FormSeparator from "../../../src/components/forms/form-horizontal/FormSeparator";
+import FormLabelAlignment from "../../../src/components/forms/form-horizontal/FormLabelAlignment";
+import CollapsibleForm from "../../../src/components/forms/form-horizontal/CollapsibleForm";
+import FormTabs from "../../../src/components/forms/form-horizontal/FormTabs";
 
 const BCrumb = [
   {
-    to: '/',
-    title: 'Home',
+    to: "/",
+    title: "Home",
   },
   {
-    title: 'Horizontal Form',
+    title: "Horizontal Form",
   },
 ];
 
@@ -29,9 +29,9 @@ const FormHorizontal = () => {
       {/* end breadcrumb */}
       <Grid container spacing={3}>
         <Grid item xs={12} lg={6}>
-          <ParentCard title="Basic Layout">
+          {/* <ParentCard title="Basic Layout">
             <BasicLayout />
-          </ParentCard>
+          </ParentCard> */}
         </Grid>
         <Grid item xs={12} lg={6}>
           <ParentCard title="Basic with Icons">
@@ -49,11 +49,15 @@ const FormHorizontal = () => {
           </ParentCard>
         </Grid>
         <Grid item xs={12}>
-          <Typography variant="h5" mb={3}>Collapsible Section</Typography>
+          <Typography variant="h5" mb={3}>
+            Collapsible Section
+          </Typography>
           <CollapsibleForm />
         </Grid>
         <Grid item xs={12}>
-          <Typography variant="h5" mb={3}>Form with Tabs</Typography>
+          <Typography variant="h5" mb={3}>
+            Form with Tabs
+          </Typography>
           <FormTabs />
         </Grid>
       </Grid>
